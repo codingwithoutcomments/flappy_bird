188e783a7c3f17a0d47948f732feeb60c17f730e
chreath <chris.x.reath.-nd@disney.com>
2014-03-31 14:48:50 -0700

    Adding graphics to the loading menu screen.

--- menu.js
+++ menu.js
@@ -1,5 +1,13 @@
 var menu_state = {  
     create: function() {
+
+        //adding the background
+        this.background1 = this.game.add.sprite(0, 0, 'bg');
+        this.background2 = this.game.add.sprite(760, 0, 'bg');
+
+        this.ground1 = this.game.add.sprite(0, 400, 'ground');
+        this.ground2 = this.game.add.sprite(763, 400, 'ground');
+
         // Call the 'start' function when pressing the spacebar
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.start, this); 
@@ -9,13 +17,13 @@ var menu_state = {
         var x = game.world.width/2, y = game.world.height/2;
 
         // Adding a text centered on the screen
-        var text = this.game.add.text(x, y-50, "Press space to start", style);
+        var text = this.game.add.text(x, y-100, "Press space to start", style);
         text.anchor.setTo(0.5, 0.5); 
 
         // If the user already played
         if (score > 0) {
             // Display its score
-            var score_label = this.game.add.text(x, y+50, "score: " + score, style);
+            var score_label = this.game.add.text(x, y, "score: " + score, style);
             score_label.anchor.setTo(0.5, 0.5); 
         }
     },
